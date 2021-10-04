b3cec887fca0b1b3ee198e0f8c8082d9e965966a
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2021-10-04 19:03:38 +0530

    Delete code.groovy

--- src/com/code.groovy
+++
@@ -1,8 +0,0 @@
-package com
-class MavenUtil implements Serializable {
-  def steps
-  MavenUtil(steps) {this.steps = steps}
-  def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
-  }
-}
