8e24045441724488b6ffa38a611c343869dc48ff
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2021-10-05 10:09:13 +0530

    Update code.groovy

--- src/com/app/code.groovy
+++ src/com/app/code.groovy
@@ -3,6 +3,6 @@ class MavenUtil implements Serializable {
   def steps
   MavenUtil(steps) {this.steps = steps}
   def mvn(args) {
-    steps.sh "${steps.tool 'Maven'}/bin/mvn -o ${args}"
+    steps.sh "/opt/apache-maven-3.6.3/bin/mvn -o ${args}"
   }
 }
