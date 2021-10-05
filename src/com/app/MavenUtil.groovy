1093533926588dec099d8057ad5cf351b5a52acf
sreejithm-hexa <84178325+sreejithm-hexa@users.noreply.github.com>
2021-10-05 10:50:57 +0530

    Create MavenUtil.groovy

---
+++ src/com/app/MavenUtil.groovy
@@ -0,0 +1,9 @@
+package com.app
+class MavenUtil implements Serializable {
+  def steps
+  MavenUtil(steps) {this.steps = steps}
+  def mvn(args) {
+    steps.sh "/opt/apache-maven-3.6.3/bin/mvn -o ${args}"
+  }
+}
+© 2021 GitHub, In
